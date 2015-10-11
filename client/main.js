822de4455ccbb0e6078cc13175434c592ec26a65
ellell <lisaovermyr@gmail.com>
2015-10-11 19:25:30 +0200

    Setup rendering result table

--- client/main.js
+++ client/main.js
@@ -1,11 +1,20 @@
 'use strict';
 
+var on = require('dom-events').on;
 var renderInputTable = require('./input-table');
-
+var renderResult = require('./result-table');
 var defaultOpts = {
   rowLabels: ['känsla', 'färg', 'form'],
   nrOfCols: 3,
-  container: document.querySelector('#input-table')
+  container: document.querySelector('#input')
 };
 
 renderInputTable(defaultOpts);
+
+var generateBtn = document.querySelector('.generate');
+on(generateBtn, 'click', function (e) {
+  e.preventDefault();
+  renderResult({
+    container: document.querySelector('#result')
+  });
+});
---
+++ client/result-table.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = function (opts) {
+  var container = opts.container;
+  container.innerHTML = 'Here is where the result should show up!';
+};
